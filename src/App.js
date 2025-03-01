9725cb762497eebde82d9f97b898ea97a63c7eea
noelw19 <noelw19@outlook.com>
2025-03-01 14:57:19 +1300

    new fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         {/* left container */}
         <div className='w-[100%] md:w-[35%] h-fit md:h-[98vh] pt-8 md:pt-16 md:pl-8 flex items-center md:items-left flex-col md:justify-between'>
 
-          <div className='flex items-center gap-2 md:block'>
+          <div className='w-full flex items-center justify-evenly gap-2 md:block'>
 
             <div className="avatar w-[150px] md:w-[250px] h-[150px] md:h-[250px] flex justify-left">
               <div className="w-full md:w-[100%] rounded-full border-4 border-white">
@@ -46,13 +46,13 @@ function App() {
             </div>
           </div>
 
-          <div className='w-full pt-4 md:pt-0'>
+          <div className='w-full pt-8 pl-4 md:p-0'>
             <p className='text-left text-base'>I want to build software with utility.</p>
             <p className='text-left text-base'>Software that helps.</p>
             <p className='text-left'></p>
           </div>
 
-          <div className='w-full pt-4 my-8 text-sm'>
+          <div className='w-full pt-4 pl-4 my-8 text-sm md:p-0'>
             <p className='text-left text-base'><span className='text-xl'>Meliorus</span> <br /> <span className='text-md py-6'>Taken from Meliorism</span> <span className='text-gray-500 text-md'> noun</span></p>
             <p className='text-left text-base'>[ me·​lio·​rism ]</p>
             <p className='text-left pt-4 p-2 text-base'>:the idea that progress is a real concept and that humans can interfere with natural processes in order to improve the world</p>
@@ -60,7 +60,7 @@ function App() {
         </div>
 
         {/* right container */}
-        <div className='w-full h-fit md:h-[98vh] pt-0 md:pt-14 pr-2 flex flex-row flex-wrap justify-evenly'>
+        <div className='w-full h-fit md:h-[98vh] pt-0 md:pt-14 pr-2 flex flex-row flex-wrap justify-evenly '>
           <Card img={img} title={"A Loved One (Alo)"} description={"A Place to store your loved ones memory"} click={() => { window.open("https://alo.meliorus.co.nz", "_blank") }} />
 
         </div>
